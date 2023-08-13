920edfe190a1ab0936d12c840f93e33df9ea2b43
Eliton <elitonkfogaca@gmail.com>
2023-08-12 21:55:15 -0300

    Meu primeiro commit do projeto ContaBanco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-import java.util.Locale;
-
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
-        
+        System.out.println("Hello World!");
     }
 }
