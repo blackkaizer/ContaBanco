3d656fa967b3b69706c20a934cb86f7c38374b78
Eliton <elitonkfogaca@gmail.com>
2023-08-12 21:52:43 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+    }
+}
