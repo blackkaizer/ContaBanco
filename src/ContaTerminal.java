c3ed336a52d7896cc7cf73fd425e699768bb07f2
Eliton <elitonkfogaca@gmail.com>
2023-08-12 22:10:00 -0300

    Criando a logica de scanner dos dados.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da Agencia:");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da conta: ");
+        String conta = scanner.next();
+        
+        System.out.println("Por favor digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
     }
 }
